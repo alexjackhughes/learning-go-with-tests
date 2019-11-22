187b22da1f7d3e5205d60f5bffcea52435c4df76
Alex Hughes <alex@alexjackhughes.com>
2019-11-22 20:10:05 +0000

    Added subtract function

--- integers/Sum.go
+++ integers/Sum.go
@@ -7,6 +7,11 @@ func Add(x, y int) (sum int) {
 	return x + y
 }
 
+// Subtract allows you to minus one number from another, and return the result.
+func Subtract(x, y int) (sum int) {
+	return x - y
+}
+
 func main() {
 	fmt.Println(Add(1, 2))
 }
