2e70a292711cdd83af1fa7bfe4784c595dba5cda
Alex Hughes <alex@alexjackhughes.com>
2019-11-22 20:10:05 +0000

    Added subtract function

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -38,3 +38,37 @@ func ExampleAdd() {
 	fmt.Println(sum)
 	// Output: 6
 }
+
+func TestMinus(t *testing.T) {
+	assert := func(t *testing.T, got, want int) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("Expected %d but we received %d.", want, got)
+		}
+	}
+
+	t.Run("4 - 1 = 3", func(t *testing.T) {
+		got := Subtract(4, 1)
+		want := 3
+		assert(t, got, want)
+	})
+
+	t.Run("1 - 5 = -4", func(t *testing.T) {
+		got := Subtract(1, 5)
+		want := -4
+		assert(t, got, want)
+	})
+
+	t.Run("60 - 13 = 47", func(t *testing.T) {
+		got := Subtract(60, 13)
+		want := 47
+		assert(t, got, want)
+	})
+}
+
+func ExampleSubtract() {
+	sum := Subtract(10, 5)
+	fmt.Println(sum)
+	// Output: 5
+}
