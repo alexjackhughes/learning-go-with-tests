9510d8722664a4c49b349f53a3f212be092d9b4b
Alex Hughes <alex@alexjackhughes.com>
2019-12-01 20:53:03 +0000

    Finished chapter on shapes

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -1,35 +1,60 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestPerimeter(t *testing.T) {
 	assert := func(t *testing.T, got, want float64) {
 		t.Helper()
 
 		if got != want {
-			t.Errorf("Expected %.2f but we received %.2f.", want, got)
+			t.Errorf("Expected %g but we received %g.", want, got)
 		}
 	}
 
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, areaTest := range areaTests {
+		got := areaTest.shape.Area()
+		want := areaTest.want
+
+		assert(t, got, want)
+	}
+
 	t.Run("Calcuate the perimeter", func(t *testing.T) {
-		got := Perimeter(10.0, 10.0)
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Perimeter()
 		want := 40.0
 
 		assert(t, got, want)
 	})
 
 	t.Run("Calculate the area of a square", func(t *testing.T) {
-		got := Area(10.0, 10.0)
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
 		want := 100.0
 
 		assert(t, got, want)
 	})
 
 	t.Run("Calculate the area of a rectangle", func(t *testing.T) {
-		got := Area(10.0, 30.0)
+		rectangle := Rectangle{10.0, 30.0}
+		got := rectangle.Area()
 		want := 300.0
+
+		assert(t, got, want)
+	})
+
+	t.Run("Calculate the area of a circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
 
 		assert(t, got, want)
 	})
